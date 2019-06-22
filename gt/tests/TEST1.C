a7d61bfc64f1a5b4871a51691d8b05f50ada13c9
frednora <freddynora@gmail.com>
2019-06-22 10:06:24 -0300

    deleting cc500

--- gt/tests/TEST1.C
+++ gt/tests/TEST1.C
@@ -1,11 +1,15 @@
 
-void main ( ) 
 
+
+/* on cc500 The first thing defined must be main(). */
+int main1();
+int main()
 {
-	
-    return ;
-    
+  return main1();
 }
 
-
+int main1()
+{	
+    return 1+2;
+}
 
